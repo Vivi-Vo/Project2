0258de42c9d222dafdefe3494d8d2f175189a815
Jonathan Kuhl <jonathankuhl@Jonathans-MacBook-Pro-2.local>
2018-08-17 14:48:15 -0400

    updated and tested uri service

--- frontend/src/app/components/services/uris.service.spec.ts
+++ frontend/src/app/components/services/uris.service.spec.ts
@@ -16,4 +16,16 @@ describe('URIService', () => {
     it('should create a proper route', inject([URIService], (service: URIService) => {
         expect(service.getBatchUri(3)).toBe('/api/project2/batch/3/tests');
     }));
+
+    it('should store all proper routes', inject([URIService], (service: URIService) => {
+        expect(service.URIs.batch).toBe('/api/project2/batch');
+        expect(service.URIs.batches).toBe('/api/project2/batches');
+        expect(service.URIs.run).toBe('/run');
+        expect(service.URIs.runLocal).toBe('http://localhost:3001/run');
+    }));
+
+
+    it('should store Angular URL constant', inject([URIService], (service: URIService) => {
+        expect(service.ANGULAR_DEFAULT).toBe('http://localhost:4200/home');
+    }));
 });
--- frontend/src/app/components/services/uris.service.ts
+++ frontend/src/app/components/services/uris.service.ts
@@ -8,14 +8,14 @@ export class URIService {
     private readonly apiRoute = '/api/project2/';
     public readonly ANGULAR_DEFAULT = 'http://localhost:4200/home';
     public URIs = {
-        batch: this.apiRoute + 'batch/',
-        batches: this.apiRoute + 'batches/',
+        batch: this.apiRoute + 'batch',
+        batches: this.apiRoute + 'batches',
         run: '/run',
         runLocal: 'http://localhost:3001/run'
     };
     constructor() {}
 
     getBatchUri(id: number): string {
-        return `${this.URIs.batch}${id}/tests`;
+        return `${this.URIs.batch}/${id}/tests`;
     }
 }
