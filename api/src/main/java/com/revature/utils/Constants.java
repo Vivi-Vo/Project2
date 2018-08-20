b8150af5e4bb34ca9970a2652bbecb23f2b3ac8d
Vivi Vo <vivivo5015@gmail.com>
2018-08-20 02:24:03 -0400

    tested add trainer - add trainer pom - modified locator file

--- api/src/main/java/com/revature/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.revature.utils;
-
-public class Constants {
-	public static final String hibernateConfigPath = "/quinn/Documents/reveture/Project2/api/src/main/resources/hibernate.cfg.xml";
-	public static final String pathToChromeExecutable = "/quinn/Documents/reveture/Project2/api/src/test/resources/chromedriver.exe";
-}
